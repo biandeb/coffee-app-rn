b81960f0d5268a1c072bb3ac74aaa30d238bb68d
Bianca Deblinger <biandeb96@gmail.com>
2024-03-20 22:18:36 -0300

    termino TabNavigator con sus respectivos iconos y screens - Falta ver BlurView que no se importa

--- App.tsx
+++ App.tsx
@@ -3,24 +3,38 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import HomeScreen from './src/screens/HomeScreen';
+import TabNavigator from './src/navigators/TabNavigator';
 import DetailsScreen from './src/screens/DetailsScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen 
-            name='Details'
-            component={DetailsScreen}
-            options={{animation: 'slide_from_bottom'}}
-            >
-            </Stack.Screen>
-        </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+        <StatusBar backgroundColor="#ffffff" />
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name='Tab'
+              component={TabNavigator}
+              options={{ animation: 'slide_from_bottom' }}
+            />
+            <Stack.Screen
+              name='Details'
+              component={DetailsScreen}
+              options={{ animation: 'slide_from_bottom' }}
+            />
+            <Stack.Screen
+              name='Payment'
+              component={PaymentScreen}
+              options={{ animation: 'slide_from_bottom' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    );
+  };
 export default App
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native'
+import CustomIcon from '../components/CustomIcon';
 const HomeScreen = () => {
   return (
     <View>
+      <Text>HomeScreen</Text>
       <Text>HomeScreen</Text>
     </View>
   )
